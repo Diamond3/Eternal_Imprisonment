c9b893de45c275bc39a04ed1b21547e13e99850e
Kęstutis Keršis <kestu.kersis@gmail.com>
2022-11-05 20:23:00 +0200

    crab movement

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Crab : MonoBehaviour
 {
+    const string LEFT = "left";
+    const string RIGHT = "right";
+
+    [SerializeField]
+    Transform castPos;
+
+    [SerializeField]
+    float baseHorizontalCastDist;
+
+    [SerializeField]
+    float baseVerticalCastDist;
+
+    string facingDirection;
+
+    Vector3 baseScale;
+
     Rigidbody2D rb2d;
     readonly float moveSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        baseScale = transform.localScale;
+        facingDirection = RIGHT;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -21,6 +39,54 @@ public class Crab : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb2d.velocity = new Vector2(moveSpeed, 0);
+        float vX = moveSpeed;
+
+        if(facingDirection == LEFT)
+        {
+            vX = -moveSpeed;
+        }
+
+
+        rb2d.velocity = new Vector2(vX, 0);
+
+        if (IsHittingWall() || IsAboutToFall())
+        {
+            ChangeFacingDirection();
+        }
+    }
+
+    void ChangeFacingDirection()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x = -transform.localScale.x;
+        transform.localScale = currentScale;
+
+        facingDirection = facingDirection == LEFT ? RIGHT : LEFT;
+    }
+
+    bool IsHittingWall()
+    {
+        float castDist = facingDirection == LEFT ? -baseHorizontalCastDist : baseHorizontalCastDist;
+
+        Vector3 targetPos = castPos.position;
+        targetPos.x += castDist;
+
+        Debug.DrawLine(castPos.position, targetPos, Color.green);
+
+        bool val = Physics2D.Linecast(castPos.position, targetPos, 1 << LayerMask.NameToLayer("Ground"));
+
+        return val;
+    }
+
+    bool IsAboutToFall()
+    {
+        Vector3 targetPos = castPos.position;
+        targetPos.y -= baseVerticalCastDist;
+
+        Debug.DrawLine(castPos.position, targetPos, Color.green);
+
+        bool val = !Physics2D.Linecast(castPos.position, targetPos, 1 << LayerMask.NameToLayer("Ground"));
+
+        return val;
     }
 }
