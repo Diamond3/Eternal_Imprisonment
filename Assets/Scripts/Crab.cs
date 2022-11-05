9b0d7bbf3a36a081bfc79a9a60912dae88577dbe
Kęstutis Keršis <kestu.kersis@gmail.com>
2022-11-05 17:34:07 +0200

    crab can do a frontflip

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Crab : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+    readonly float moveSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(moveSpeed, 0);
     }
 }
