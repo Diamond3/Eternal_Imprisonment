1fc1575998fbc0adf45bd4b84c2aa049545f460c
Kęstutis Keršis <kestu.kersis@gmail.com>
2022-11-29 21:04:04 +0200

    improved level end

--- Assets/LevelEndScript.cs
+++ Assets/LevelEndScript.cs
@@ -6,7 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelEndScript : MonoBehaviour
 {
-    const string LEVEL_END_TEXT = "PRESS ENTER";
+    const string LEVEL1_END_TEXT = "January 5th, 2005:\nI need money but this gravedigging job is killing me… I can’t stop though, I need to finish my invention, it’s going to change how we perceive the world.I can’t wait to see what heaven looks like and talk to my family again.It’s been so long since I’ve seen them.Every time I dig a grave I see the pain in the people’s eyes, the same pain that I’ve been feeling for so long… I hope I can help them meet their loved ones again.\nPress Enter to continue.";
+    const string LEVEL2_END_TEXT = "January 6th, 2005:\nMet a strange man today, he seemed to know about my work...Offered me a lot of money to help me finish my invention but had weird conditions.I mean, who wants to see hell? Why would he want me to open the door to it as well? I don‘t know about this but I do need to reach heaven, I guess I‘ll take his money and worry about the consequences later.\nPress Enter to continue.";
+    const string LEVEL3_END_TEXT = "March 28th, 2005:\nI finally did it, I finally managed to open the door to the afterlife! I peeked through to hell, oh man, that place looks terrible, I need to tell my financer that it is not a good idea to go there. I’m sure that when he sees the contrast between the door to heaven and the door to hell, he’ll make the right decision. Off I go…\nPress Enter to continue.";
 
     [SerializeField]
     GameObject _endLevelPanel;
@@ -54,22 +56,54 @@ public class LevelEndScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Return))
             {
-                //TODO: parasyti kito levelio pavadinima cia
-                //SceneManager.LoadScene("TODO:NEXT_LEVEL_NAME", LoadSceneMode.Single);
+                var currentScene = SceneManager.GetActiveScene().name;
+                switch (currentScene)
+                {
+                    case "AidasScene":
+                    case "Level1":
+                        SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+                        break;
+                    case "Level2":
+                        SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+                        break;
+                    case "Level3":
+                        SceneManager.LoadScene("Level4-Boss", LoadSceneMode.Single);
+                        break;
+                    default:
+                        Application.Quit();
+                        break;
+                }
             }
         }
     }
 
     private IEnumerator TypeText()
     {
-        foreach (char letter in LEVEL_END_TEXT)
+        string levelEndText = "";
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "AidasScene":
+            case "Level1":
+                levelEndText = LEVEL1_END_TEXT;
+                break;
+            case "Level2":
+                levelEndText = LEVEL2_END_TEXT;
+                break;
+            case "Level4-Boss":
+                levelEndText = LEVEL3_END_TEXT;
+                break;
+            default:
+                Application.Quit();
+                break;
+        }
+        foreach (char letter in levelEndText)
         {
             _textMeshProUGUI.text += letter;
             // TODO: jei noresim ideti typinimo soundus:
             //if (typeSound1 && typeSound2)
             //    SoundManager.instance.RandomizeSfx(typeSound1, typeSound2);
             yield return 0;
-            yield return new WaitForSeconds(0.07f);
+            yield return new WaitForSeconds(0.03f);
         }
     }
 }
